9aa61bbf1f679e24ccdbd7d29d58a0e3e37de2d9
wanishoaib <72087077+wanishoaib@users.noreply.github.com>
2020-10-01 22:35:29 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>FLYERS & BROCHUERS SERVICE</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=width-device, initial-scale=0.1">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+</head>
+<body>
+<!--navbar start-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">FLYERS & BROCHERS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">SERVICES</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">GALLERY</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT US</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<!--navbar ends-->
+<!--slider start-->
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!--slider ends-->
+<!--about start-->
+<section class="my-5">
+    <div class="py-5">
+	    <h2 class="text-center">ABOUT US</h2>
+	</div>
+	<div class="container-fluid">
+	<div class="row">
+	    <div class="col-lg-6 col-md-6 col-12">
+		    <img src="3.jpg" class="img-fluid aboutimg">
+		</div>
+		 <div class="col-lg-6 col-md-6 col-12">
+		   <h2 class="display-4">FLYERS & BROCHURES</h2>
+		   <P class="py-3">WE HAVE THE MOST CREATIVE AND UNIQUE FLYERS AND BROCHUERS.
+		   WE CAN CREATE ANY KIND OF FLYER OR BROCHUERS AS PER CUSTOMER DEMAND.
+		   FOR FURTHER DETAILS OR BOOKING YOU CAN CONTACT US NOW.</P>
+		   <a href="contact" class="btn btn-success">BOOK NOW</a>
+		</div> 
+    </div>
+	</div>
+</section>
+<!--about ends-->
+<!--servics starts-->
+<section class="my-5">
+    <div class="py-5">
+	    <h2 class="text-center">OUR SERVICES</h2>
+	</div>
+	<div class="container-fluid">
+	    <div class="row">
+		    <div class="col-lg-4 col-md-4 col-12">
+		        <div class="card">
+                    <img class="card-img-top" src="1.JPG">
+                    <div class="card-body">
+                        <h5 class="card-title">FLYER 1</h5>
+                        <p class="card-text">WE HAVE THE MOST CREATIVE AND UNIQUE FLYERS AND BROCHUERS.
+		                    WE CAN CREATE ANY KIND OF FLYER OR BROCHUERS AS PER CUSTOMER DEMAND.
+	                    </p>
+                        <a href="#" class="btn btn-primary">OPEN</a>
+                    </div>
+                </div>
+			</div>
+			 <div class="col-lg-4 col-md-4 col-12">
+		        <div class="card">
+                    <img class="card-img-top" src="2.JPG">
+                    <div class="card-body">
+                        <h5 class="card-title">FLYER 1</h5>
+                        <p class="card-text">WE HAVE THE MOST CREATIVE AND UNIQUE FLYERS AND BROCHUERS.
+		                    WE CAN CREATE ANY KIND OF FLYER OR BROCHUERS AS PER CUSTOMER DEMAND.
+	                    </p>
+                        <a href="#" class="btn btn-primary">OPEN</a>
+                    </div>
+                </div>
+			</div>
+			 <div class="col-lg-4 col-md-4 col-12">
+		        <div class="card">
+                    <img class="card-img-top" src="3.JPG">
+                    <div class="card-body">
+                        <h5 class="card-title">FLYER 1</h5>
+                        <p class="card-text">WE HAVE THE MOST CREATIVE AND UNIQUE FLYERS AND BROCHUERS.
+		                    WE CAN CREATE ANY KIND OF FLYER OR BROCHUERS AS PER CUSTOMER DEMAND.
+	                    </p>
+                        <a href="#" class="btn btn-primary">OPEN</a>
+                    </div>
+                </div>
+			</div>
+	    </div>
+	</div>
+</section>
+<!--servics ends-->
+<!--gallery starts-->
+<section class="my-5">
+    <div class="py-5">
+	    <h2 class="text-center">OUR GALLERY</h2>
+	</div>
+	<div class="container-fluid">
+	    <div class="row">
+		    <div class="col-lg-4 col-md-4 col-12">
+			    <img src="1.JPG" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+			    <img src="2.JPG" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+			    <img src="3.JPG" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+			    <img src="2.JPG" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+			    <img src="3.JPG" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+			    <img src="1.JPG" class="img-fluid pb-4">
+			</div>
+			
+	    </div>
+	</div>
+</section>
+<!--gallery ends-->
+<!--contact us start-->
+<section class="my-5">
+    <div class="py-5">
+	    <h2 class="text-center">CONTACT US</h2>
+	</div>
+	<div class="w-50 m-auto">
+	    <form action="userinfo.php" method="post">
+		    <div class="form-group">
+			    <label>USERNAME</label>
+				<input type="text" name="user" placeholder="username" required class="form-control">
+			</div>
+			 <div class="form-group">
+			    <label>EMAIL ADRESS</label>
+				<input type="text" name="email" placeholder="email@adress.com" required class="form-control">
+			</div>
+			 <div class="form-group">
+			    <label>MOBILE NUMBER</label>
+				<input type="text" name="mobile" placeholder="+92132546978" required class="form-control">
+			</div>
+			 <div class="form-group">
+			    <label>COMMENT</label>
+				<textarea class="form-control" name="comment">
+				</textarea>
+			</div>
+			<button type="submit" class="btn btn-success">SUBMIT</button>
+		</form>
+	</div>
+</section>
+<!--contact us ends-->
+<footer>
+		    <p class="p-3 bg-dark text-white text-center">copyright &#169; 2020 FLYERS & BROCHUERS pvt.ltd</p>
+		</div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ userinfo.php
@@ -0,0 +1,21 @@
+<?php
+$con = mysqli_connect('localhost','root');
+if($con){
+	echo"connection successful";
+}else{
+	echo"no connection";
+}
+mysqli_select_db($con,'flyerdb');
+$user =$_POST['user'];
+$email =$_POST['email'];
+$mobile =$_POST['mobile'];
+$comment =$_POST['comment'];
+
+$query = "insert into userinfodata(user, email, mobile, comment)
+values('$user','$email','$mobile','$comment')";
+
+echo "$query";
+mysqli_query($con,$query);
+header('location:index.php');
+
+?>
